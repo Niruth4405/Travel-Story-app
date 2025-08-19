e9c9d59c309728c1d237eb3697161b55c89507c1
Niruth Ananth <niruthananth03@gmail.com>
2025-08-20 01:15:18 +0530

    .

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,8 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 // import { BASE_URL } from "./constants"
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: process.env.REACT_APP_BASE_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
